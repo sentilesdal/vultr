75e5dbe70653b3472d095a990d2433837383b464
Matt Brown <mattfdbrown@gmail.com>
2015-04-28 07:04:28 -0700

    Add firebase form, facebook, google auth

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -41,6 +41,33 @@ angular.module('vultr',[])
     $scope.header = modal.header;
     $scope.footer = modal.footer;
   });
+
+  $scope.authRedirect = function(provider){
+    ref.authWithOAuthRedirect(provider, authHandler);
+  };
+    
+  $scope.authForm = function(){
+    ref.createUser({
+      email: $scope.userEmail,
+      password: $scope.userPassword 
+    }, function(error, userData) {
+      if (error) {
+        switch (error.code) {
+          case "EMAIL_TAKEN":
+            console.log("The new user account cannot be created because the email is already in use.");
+            break;
+          case "INVALID_EMAIL":
+            console.log("The specified email is not a valid email.");
+            break;
+          default:
+            console.log("Error creating user:", error);
+        }
+      } else {
+        console.log("Successfully created user account with uid:", userData.uid);
+      }
+    });
+  };
+  
   
 }])
 
@@ -127,22 +154,3 @@ function getName(authData) {
   }
 }
 
-ref.createUser({
-  email: "bobtony@firebase.com",
-  password: "correcthorsebatterystaple"
-}, function(error, userData) {
-  if (error) {
-    switch (error.code) {
-      case "EMAIL_TAKEN":
-        console.log("The new user account cannot be created because the email is already in use.");
-        break;
-      case "INVALID_EMAIL":
-        console.log("The specified email is not a valid email.");
-        break;
-      default:
-        console.log("Error creating user:", error);
-    }
-  } else {
-    console.log("Successfully created user account with uid:", userData.uid);
-  }
-});
