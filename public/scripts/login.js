c6ddd1a9462c6ccc8496c8f05da6e9172133cd68
Matt Brown <mattfdbrown@gmail.com>
2015-04-27 23:43:09 -0700

    add fa icons to modal.  Add fa to bower

---
+++ public/scripts/login.js
@@ -0,0 +1,32 @@
+var ref = new Firebase("https://<your-firebase>.firebaseio.com");
+var authData = ref.getAuth();
+
+if (authData) {
+  console.log("User " + authData.uid + " is logged in with " + authData.provider);
+} else {
+  console.log("User is logged out");
+}
+// Create a callback to handle the result of the authentication
+function authHandler(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+}
+
+// Authenticate users with a custom Firebase token
+//ref.authWithCustomToken("<token>", authHandler);
+
+// Alternatively, authenticate users anonymously
+//ref.authAnonymously(authHandler);
+
+// Or with an email/password combination
+ref.authWithPassword({
+  email    : 'bobtony@firebase.com',
+  password : 'correcthorsebatterystaple'
+}, authHandler);
+
+// Or via popular OAuth providers ("facebook", "github", "google", or "twitter")
+ref.authWithOAuthPopup("facebook", authHandler);
+ref.authWithOAuthRedirect("google", authHandler);
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var Firebase = require('firebase');
 
 var app = express();
-var data = new Firebase('');
+//var data = new Firebase('');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
