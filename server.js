3b3116d1c48c8d8896929e6bb27e575bd0350d55
Matt Brown <mattfdbrown@gmail.com>
2015-04-27 19:21:20 -0700

    Add modal, navbar submit/search

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+var Firebase = require('firebase');
+
 var app = express();
+var data = new Firebase('');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+
 
 app.listen(app.get('port'));
 
