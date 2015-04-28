125552230ef55afc94696e4f715e332cb9b9d1eb
Matt Brown <mattfdbrown@gmail.com>
2015-04-27 17:16:24 -0700

    add Procfile

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-//app.get('/', function(req, res){
-//    res.end('Hello World!');
-//});
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 app.listen(3000);
 
