6d24ae0ba000c6310219b115dc2ec6af066e04b6
Matt Brown <mattfdbrown@gmail.com>
2015-04-28 12:19:08 -0700

    Add a bunch of tweeks

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,74 +1,179 @@
 angular.module('vultr',[])
 
-.factory('modal', ['$rootScope', function($rootScope){
+.factory('auth', ['$rootScope', '$timeout', function($rootScope, $timeout){
+  var ref = new Firebase("https://blistering-heat-2157.firebaseio.com");
+  var isNewUser = false;
+
+
+  // Create a callback to handle the result of the authentication
+  var authHandler = function(error, authData) {
+    if (error) {
+      services.authErrMsg = error.message;
+      $rootScope.$broadcast('auth:error');
+      $rootScope.$broadcast('auth:unauth');
+      console.log("Login Failed!", error);
+    } else {
+      $rootScope.$broadcast('auth:success');
+      $('#myModal').modal('hide');
+      console.log("Authenticated successfully with payload:", authData);
+    }
+  };
+
   var services = {};
-  
+
+  services.authData = ref.getAuth();
+  services.loggedin = false;
   services.header = '';
   services.footer = '';
+  services.authErrMsg = '';
 
   services.login = function(){
     services.header = 'Log In';
     services.footer = 'Not a user? Signup';
-    $rootScope.$broadcast('modal:updated');
+    $rootScope.$broadcast('auth:updated');
+    $('#myModal').modal('show');
+  };
+
+  services.logout = function(value){
+    ref.unauth();
+    $rootScope.$broadcast('auth:unauth');
   };
 
   services.signup= function(){
     services.header = 'Sign up';
     services.footer = 'Already a user? Login';
-    $rootScope.$broadcast('modal:updated');
+    $rootScope.$broadcast('auth:updated');
+    $('#myModal').modal('show');
   };
-    
+   
+  services.authRedirect = function(provider){
+    ref.authWithOAuthRedirect(provider, authHandler);
+  };
+
+  services.authForm = function(userEmail, userPassword){
+    ref.authWithPassword({
+        email    :  userEmail,
+        password :  userPassword
+      }, authHandler);
+  };
+
+  services.createUser = function(userEmail, userPassword){
+    ref.createUser({
+      email: userEmail,
+      password: userPassword 
+    }, function(error, userData) {
+      if (error) {
+        services.authErrMsg = error.message;
+        console.log(services.authErrMsg);
+        $rootScope.$broadcast('auth:error');
+      } else {
+        console.log("Successfully created user account with uid:", userData.uid);
+        $('#myModal').modal('hide');
+        $timeout(function(){
+          console.log('HEELLLOOO');
+          $rootScope.$broadcast('auth:success');
+        },3000);
+        isNewUser = true;
+      }
+    });
+  };
+
+  ref.onAuth(function(authData) {
+    if (authData && isNewUser) {
+      ref.child("users").child(authData.uid).set({
+        provider: authData.provider,
+        name: getName(authData)
+      });
+    }
+  });
+
+  services.getName = function() {
+    switch(services.authData.provider) {
+       case 'password':
+         return services.authData.password.email.replace(/@.*/, '');
+       case 'google':
+         return services.authData.google.displayName;
+       case 'facebook':
+         return services.authData.facebook.displayName;
+    }
+  };
+
 
   return services;
 }])
 
-.controller('HeaderController', ['$scope', 'modal', function($scope, modal){
-  $scope.login = modal.login;
-  $scope.signup = modal.signup;
+.controller('HeaderController', ['$scope', 'auth', function($scope, auth){
+  $scope.loggedin = false;
 
-}])
+  if (auth.authData) {
+    console.log("User " + auth.authData.uid + " is logged in with " + auth.authData.provider);
+    $scope.loggedin = true;
+  } else {
+    console.log("User is logged out");
+  }
 
-.controller('ModalController', ['$scope', 'modal', function($scope, modal){
-
-  $scope.toggle = function(){
-    if (modal.header === 'Log In')
-      modal.signup();
-    else
-      modal.login();
-  };
-
-  $scope.$on('modal:updated', function(){
-    $scope.header = modal.header;
-    $scope.footer = modal.footer;
+  $scope.$on('auth:sucess', function(){
+    console.log('hey there');
+    $timeout(function(){
+      $scope.loggedin = true;
+    },5000);
   });
 
-  $scope.authRedirect = function(provider){
-    ref.authWithOAuthRedirect(provider, authHandler);
+  $scope.$on('auth:unauth', function(){
+    $scope.loggedin = false;
+  });
+
+  $scope.login = function(){
+    $scope.loggedin = true;
+    auth.login();
   };
+  $scope.logout = auth.logout;
+  $scope.signup = auth.signup;
+}])
+
+.controller('ModalController', ['$scope', 'auth', function($scope, auth){
+
+  $scope.authErr = false;
+  $scope.authErrMsg = '';
+  $scope.alertType = 'alert-success';
+
+  $scope.toggle = function(){
+    if (auth.header === 'Log In')
+      auth.signup();
+    else
+      auth.login();
+  };
+ 
+  $scope.$on('auth:error', function(){
+    $scope.authErrMsg = auth.authErrMsg;
+    console.log(auth.authErrMsg);
+    $('.modal-body .alert').remove();
+    $('.modal-body').prepend('<div ng-show="authErr" ng-model="alertType" class="alert alert-danger" role="alert">'+$scope.authErrMsg+' </div>').animate('fast');
+    $('#myModal').modal('show');
+    setTimeout(function(){
+      $('.modal-body .alert').remove();
+    },5000);
+    $scope.authErr = false;
+  });
+
+  $scope.$on('auth:success', function(){
+    $('.modal-body .alert').remove();
+  });
+    
+  $scope.$on('auth:updated', function(){
+    $scope.header = auth.header;
+    $scope.footer = auth.footer;
+  });
+
+  $scope.authRedirect = auth.authRedirect;
     
   $scope.authForm = function(){
-    ref.createUser({
-      email: $scope.userEmail,
-      password: $scope.userPassword 
-    }, function(error, userData) {
-      if (error) {
-        switch (error.code) {
-          case "EMAIL_TAKEN":
-            console.log("The new user account cannot be created because the email is already in use.");
-            break;
-          case "INVALID_EMAIL":
-            console.log("The specified email is not a valid email.");
-            break;
-          default:
-            console.log("Error creating user:", error);
-        }
-      } else {
-        console.log("Successfully created user account with uid:", userData.uid);
-      }
-    });
+    if ($scope.header === "Log In"){
+      auth.authForm($scope.userEmail, $scope.userPassword);
+    } else {
+      auth.createUser($scope.userEmail, $scope.userPassword);
+    }
   };
-  
-  
 }])
 
 .controller('LinksController', ['$scope', function($scope){
@@ -90,67 +195,4 @@ angular.module('vultr',[])
     $scope.links[i].votes--;
   };
 }]);
-
-var ref = new Firebase("https://blistering-heat-2157.firebaseio.com");
-var authData = ref.getAuth();
-
-if (authData) {
-  console.log("User " + authData.uid + " is logged in with " + authData.provider);
-} else {
-  console.log("User is logged out");
-}
-
-// Create a callback to handle the result of the authentication
-function authHandler(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-    console.log("Login Failed!", authData);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
-}
-
-// Authenticate users with a custom Firebase token
-//ref.authWithCustomToken("<token>", authHandler);
-
-// Alternatively, authenticate users anonymously
-//ref.authAnonymously(authHandler);
-
-// Or with an email/password combination
-//ref.authWithPassword({
-//  email    : 'bobtony@firebase.com',
-//  password : 'correcthorsebatterystaple'
-//}, authHandler);
-
-// Or via popular OAuth providers ("facebook", "github", "google", or "twitter")
-//ref.authWithOAuthRedirect("facebook", authHandler);
-//ref.authWithOAuthRedirect("google", authHandler);
-
-// we would probably save a profile when we register new users on our site
-// we could also read the profile to see if it's null
-// here we will just simulate this with an isNewUser boolean
-var isNewUser = true;
-
-ref.onAuth(function(authData) {
-  if (authData && isNewUser) {
-    // save the user's profile into Firebase so we can list users,
-    // use them in Security and Firebase Rules, and show profiles
-    ref.child("users").child(authData.uid).set({
-      provider: authData.provider,
-      name: getName(authData)
-    });
-  }
-});
-
-// find a suitable name based on the meta info given by each provider
-function getName(authData) {
-  switch(authData.provider) {
-     case 'password':
-       return authData.password.email.replace(/@.*/, '');
-     case 'twitter':
-       return authData.twitter.displayName;
-     case 'facebook':
-       return authData.facebook.displayName;
-  }
-}
 
